091f553f67cc21a346b20ef52437d7aba83d0d7a
Andres Vaccari <andresvaccari34@gmail.com>
2023-09-09 02:30:13 -0300

    Cambio input

--- components/PlayerSelector.jsx
+++ components/PlayerSelector.jsx
@@ -1,5 +1,5 @@
-import { useCallback, useState, useRef } from "react";
-import { SearchList } from "./SearchList";
+import { AiFillAlert } from "react-icons/ai";
+import AsyncSelect from "react-select/async";
 
 export default function PlayerSelector({
   index,
@@ -7,62 +7,33 @@ export default function PlayerSelector({
   player,
   errorId,
 }) {
-  const [selectedPlayer, setSelectedPlayer] = useState(null);
-  const [results, setResults] = useState([]);
-  const inputRef = useRef(null);
-
-  const searchEndpoint = (query) =>
-    `https://cors-anywhere-andresvaccari.onrender.com/https://api.beatleader.xyz/players?sortBy=pp&page=1&count=50&search=${query}&mapsType=ranked&ppType=general&friends=false`;
-
-  const search = useCallback(async (event) => {
-    setResults([]);
-    const searchTerm = event.target.value;
-    if (searchTerm.trim() !== "" && searchTerm.length > 3) {
-      try {
-        const response = await fetch(searchEndpoint(searchTerm));
-        const { data } = await response.json();
-        setResults(data);
-      } catch (error) {
-        setResults([]);
-      }
-    } else {
-      setResults([]);
-    }
-  }, []);
-
-  const handleResultClick = (result) => {
-    handleChange(index, result);
-    setSelectedPlayer(result);
-    inputRef.current.value = result.name;
-    setResults([]);
+  const search = async (inputValue) => {
+    const res = await fetch(
+      `https://cors-anywhere-andresvaccari.onrender.com/https://api.beatleader.xyz/players?sortBy=pp&page=1&count=50&search=${inputValue}&mapsType=ranked&ppType=general&friends=false`
+    );
+    const { data } = await res.json();
+    console.log(data);
+    return data.map((item) => ({
+      label: item.name + " - " + item.country,
+      value: item,
+    }));
   };
 
   return (
     <div className="grow">
-      <input
-        type="text"
-        id="beatleader-player"
-        name="beatleader-player"
-        className={`text-black w-full border-2 rounded-md p-2 ${
+      <AsyncSelect
+        loadOptions={search}
+        onChange={(result) => {
+          handleChange(index, result.value);
+        }}
+        placeholder="Search for a player..."
+        noOptionsMessage={() => "No players found"}
+        defaultOptions={false}
+        required
+        className={`text-black w-full rounded-md ${
           errorId == index && "border-red-500"
         }`}
-        placeholder="Search for a player..."
-        title="Search for a player..."
-        required
-        autoComplete="off"
-        defaultValue={player?.name || ""}
-        onChange={search}
-        onClick={(e) => {
-          setSelectedPlayer(null);
-          search(e);
-          inputRef.current.value = "";
-          inputRef.current.focus();
-        }}
-        ref={inputRef}
       />
-      {results.length > 0 && !selectedPlayer && (
-        <SearchList results={results} onClick={handleResultClick} />
-      )}
     </div>
   );
 }
