463488c5f67abe820c8efa4f267b587518ca6667
Andrés Juan Vaccari <93448866+AndresVaccari@users.noreply.github.com>
2023-11-05 20:54:17 -0300

    Update hello.js

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -4,4 +4,4 @@ export default function handler(req, res) {
   res.status(200).json({ name: "John Doe" });
 }
 
-export const PROXY = "https://63e7-186-13-96-151.ngrok-free.app";
+export const PROXY = "https://cae2-186-13-96-151.ngrok-free.app";
